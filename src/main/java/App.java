b4a1b6255244155773fad8cb6251a3931d5d0fa1
Bhanu-Vallabhaneni <vbhanuprakash46@gmail.com>
2020-05-09 18:11:57 +0530

    test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,13 +4,14 @@ public class App {
     	return n1+n2;
     }
     public static void main(String args[]) {
-    	int n1, n2, res;
-    	Scanner sc = new Scanner(System.in);
-    	System.out.println("Enter number:");
-    	n1 = sc.nextInt();
-    	System.out.println("Enter number:");
-    	n2 = sc.nextInt();
-			res = add(n1,n2);
-    	System.out.println("Sum is: " + res);
+    	// int n1, n2, res;
+    	// Scanner sc = new Scanner(System.in);
+    	// System.out.println("Enter number:");
+    	// n1 = sc.nextInt();
+    	// System.out.println("Enter number:");
+    	// n2 = sc.nextInt();
+			// res = add(n1,n2);
+    	// System.out.println("Sum is: " + res);
+    	System.out.println("helloworld");
     }
 }
