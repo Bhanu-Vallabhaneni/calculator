0210e40ac5a4441a7c8a7abe1da9a2477217613d
Bhanu-Vallabhaneni <vbhanuprakash46@gmail.com>
2020-05-10 09:43:39 +0530

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,21 @@
-import java.util.Scanner;
+
 public class App {
-    public static int add(int n1,int n2){
-    	return n1+n2;
-    }
+   
     public static void main(String args[]) {
-    	// int n1, n2, res;
-    	// Scanner sc = new Scanner(System.in);
-    	// System.out.println("Enter number:");
-    	// n1 = sc.nextInt();
-    	// System.out.println("Enter number:");
-    	// n2 = sc.nextInt();
-			// res = add(n1,n2);
-    	// System.out.println("Sum is: " + res);
-    	System.out.println("helloworld");
+    	 int n1, n2, sum,dif,mul,div;
+    	 
+    	 n1 = 4;
+    	
+    	 n2 = 2;
+		 sum = n1+n2;
+		 System.out.println("numbers are: " + n1 +  "and"  + n2);
+    	 System.out.println("Sum  is: " + sum);
+    	 dif = n1-n2;
+    	 System.out.println("difference  is: " + dif);
+    	 mul = n1*n2;
+    	 System.out.println("multipy is: " + mul);
+    	 div = n1/n2;
+    	 System.out.println("division  is: " + div);
+    	
     }
 }
